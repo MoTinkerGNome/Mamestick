49ec4d0dc0fd7b4c17fa16e4e5ba65fc2b981141
MoTinkerGNome <carlcampbelljr@yahoo.com>
2014-10-07 22:07:35 -0500

    Update msdrive.py
    fixed syntax

--- msdrive.py
+++ msdrive.py
@@ -40,59 +40,47 @@ start=7	#number key 6
 
 while True:
     joy_x_value=ReadChannel(joy_x)
-		#print ("Joy X Value:{}".format(joy_x_value))
-		device.emit(uinput.ABS_X,joy_x_value,syn=False)
+#print ("Joy X Value:{}".format(joy_x_value))
+device.emit(uinput.ABS_X,joy_x_value,syn=False)
 
-    joy_y_value=ReadChannel(joy_y)
-		#print ("Joy Y Value:{}".format(joy_y_value))
-		device.emit(uinput.ABS_Y,joy_y_value)
+joy_y_value=ReadChannel(joy_y)
+#print ("Joy Y Value:{}".format(joy_y_value))
+device.emit(uinput.ABS_Y,joy_y_value)
 
-    coin_value=ReadChannel(coin)
-    if coin_value==1:
-		#print ("Button Pressed {}".format(coin_value))
-		device.emit_click(uinput.KEY_1,1)
-    else:
-		#print ("Button Not Pressed {}".format(coin_value))
-		device.emit_click(uinput.KEY_1,0)
+coin_value=ReadChannel(coin)
+if coin_value==1:
+    #print ("Button Pressed {}".format(coin_value))
+    device.emit_click(uinput.KEY_1,1)
+		
+ls_value=ReadChannel(ls)
+if ls_value==1:
+    #print ("Button Pressed {}".format(ls_value))
+    device.emit_click(uinput.KEY_2,1)
+    
+	  
+rs_value=ReadChannel(rs)
+if rs_value==1:
+    #print ("Button Pressed {}".format(rs_value))
+    device.emit_click(uinput.KEY_3,1)
+    
+		
+	  
+esc_value=ReadChannel(esc)
+if esc_value==1:
+    #print ("Button Pressed {}".format(esc_value))
+    device.emit_click(uinput.KEY_4,1)
+   
+	  
+sel_value=ReadChannel(sel)
+if sel_value==1:
+    #print ("Button Pressed {}".format(sel_value))
+    device.emit_click(uinput.KEY_5,1)
 
-    ls_value=ReadChannel(ls)
-    if ls_value==1:
-		#print ("Button Pressed {}".format(ls_value))
-		device.emit_click(uinput.KEY_2,1)
-    else:
-		#print ("Button Not Pressed {}".format(ls_value))
-		device.emit_click(uinput.KEY_2,0)
+
 	  
-	rs_value=ReadChannel(rs)
-    if rs_value==1:
-		#print ("Button Pressed {}".format(rs_value))
-		device.emit_click(uinput.KEY_3,1)
-    else:
-		#print ("Button Not Pressed {}".format(rs_value))
-		device.emit_click(uinput.KEY_3,0)
-	  
-	esc_value=ReadChannel(esc)
-    if esc_value==1:
-		#print ("Button Pressed {}".format(esc_value))
-		device.emit_click(uinput.KEY_4,1)
-    else:
-		#print ("Button Not Pressed {}".format(esc_value))
-		device.emit_click(uinput.KEY_4,0) 
-	  
-	sel_value=ReadChannel(sel)
-    if sel_value==1:
-		#print ("Button Pressed {}".format(sel_value))
-		device.emit_click(uinput.KEY_5,1)
-    else:
-		#print ("Button Not Pressed {}".format(sel_value))
-		device.emit_click(uinput.KEY_5,0)
-	  
-	start_value=ReadChannel(start)
-    if start_value==1:
-		#print ("Button Pressed {}".format(start_value))
-		device.emit_click(uinput.KEY_6,1)
-    else:
-		#print ("Button Not Pressed {}".format(start_value))
-		device.emit_click(uinput.KEY_6,0)
-	  
-    time.sleep(0.020) 
+start_value=ReadChannel(start)
+if start_value==1:
+    #print ("Button Pressed {}".format(start_value))
+    device.emit_click(uinput.KEY_6,1)
+    
+time.sleep(0.020) 
